6e89c5f57c2f66f25fedf1020b0e13a46f3be95b
Tyler Long <tyler4long@gmail.com>
2021-07-01 10:10:48 -0700

    Frist working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello world!');
+import SDK from '@ringcentral/sdk';
+
+const sdk = new SDK({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+  server: process.env.RINGCENTRAL_SERVER_URL!,
+});
+
+(async () => {
+  await sdk.login({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION!,
+    password: process.env.RINGCENTRAL_PASSWORD!,
+  });
+
+  const endpoint = '/restapi/v1.0/account/~/extension/~';
+
+  const r = await sdk.send({
+    method: 'get',
+    url: endpoint,
+    body: undefined,
+    query: undefined,
+    headers: undefined,
+  });
+
+  console.log(r.url.split(endpoint)[0]);
+})();
